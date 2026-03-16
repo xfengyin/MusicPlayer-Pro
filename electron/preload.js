0d1bb72c33893e65de53627152c9d33a136b796d
xfengyin <55651142+xfengyin@users.noreply.github.com>
2026-03-16 14:35:02 +0800

    feat: 完善 electron preload

--- electron/preload.js
+++ electron/preload.js
@@ -1,6 +1,18 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  getAppPath: () => ipcRenderer.invoke('get-app-path'),
-  platform: process.platform
+  getAppVersion: () => ipcRenderer.invoke('get-app-version'),
+  getPlatform: () => ipcRenderer.invoke('get-platform'),
+  showOpenDialog: (options) => ipcRenderer.invoke('show-open-dialog', options),
+  
+  // 播放器控制
+  onPlay: (callback) => ipcRenderer.on('player-play', callback),
+  onPause: (callback) => ipcRenderer.on('player-pause', callback),
+  onNext: (callback) => ipcRenderer.on('player-next', callback),
+  onPrevious: (callback) => ipcRenderer.on('player-previous', callback),
+  
+  // 系统
+  minimize: () => ipcRenderer.send('window-minimize'),
+  maximize: () => ipcRenderer.send('window-maximize'),
+  close: () => ipcRenderer.send('window-close')
 });
