8db1ff4d24ad68bb17f28d0690b317cf3d94a58c
xfengyin <55651142+xfengyin@users.noreply.github.com>
2026-03-24 09:58:47 +0800

    feat: add secure preload script with contextBridge API

--- electron/preload.js
+++ electron/preload.js
@@ -1,18 +1,38 @@
+/**
+ * Preload Script
+ * 安全地暴露 API 给渲染进程
+ */
+
 const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('electronAPI', {
-  getAppVersion: () => ipcRenderer.invoke('get-app-version'),
-  getPlatform: () => ipcRenderer.invoke('get-platform'),
-  showOpenDialog: (options) => ipcRenderer.invoke('show-open-dialog', options),
+// 定义暴露的 API
+const api = {
+  // 应用信息
+  app: {
+    getVersion: () => ipcRenderer.invoke('app:getVersion'),
+    getPlatform: () => ipcRenderer.invoke('app:getPlatform'),
+  },
   
-  // 播放器控制
-  onPlay: (callback) => ipcRenderer.on('player-play', callback),
-  onPause: (callback) => ipcRenderer.on('player-pause', callback),
-  onNext: (callback) => ipcRenderer.on('player-next', callback),
-  onPrevious: (callback) => ipcRenderer.on('player-previous', callback),
+  // 文件对话框
+  dialog: {
+    openFile: () => ipcRenderer.invoke('dialog:openFile'),
+  },
   
-  // 系统
-  minimize: () => ipcRenderer.send('window-minimize'),
-  maximize: () => ipcRenderer.send('window-maximize'),
-  close: () => ipcRenderer.send('window-close')
-});
+  // 事件监听
+  onAppError: (callback) => {
+    ipcRenderer.on('app-error', (event, error) => callback(error));
+  },
+  
+  // 移除事件监听
+  removeAllListeners: (channel) => {
+    ipcRenderer.removeAllListeners(channel);
+  },
+};
+
+// 安全地暴露 API
+contextBridge.exposeInMainWorld('electronAPI', api);
+
+// 类型声明（供 TypeScript 使用）
+if (process.env.NODE_ENV === 'development') {
+  console.log('[Preload] API exposed securely');
+}
